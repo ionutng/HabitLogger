88854bbe8cd380db222b3f5197be68db9248285e
ionutng <ionutngp@gmail.com>
2023-09-26 11:25:05 +0300

    Create table if it doesn't exist already

--- HabitLogger/Program.cs
+++ HabitLogger/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Data.Sqlite;
+
+using (var connection = new SqliteConnection("Data Source=habit-logger.db"))
+{
+    connection.Open();
+
+    var command = connection.CreateCommand();
+    command.CommandText = 
+        @"
+            CREATE TABLE IF NOT EXISTS drinking_water (
+                Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                Date TEXT,
+                Quantity INTEGER)
+        ";
+
+    command.ExecuteNonQuery();
+
+    connection.Close();
+}
